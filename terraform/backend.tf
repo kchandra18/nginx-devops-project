687a77d16b2d92179bddfd4e2e4e1ea898b6c2af
kchandra18 <chandrasekhar75168@gmail.com>
2026-01-08 11:24:10 +0530

    Enable remote backend for Terraform state

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "chandbjs5fn5sek"
-#     key            = "nginx/terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "terraform-lock"
-#     encrypt        = true
-#   }
-# }
+ terraform {
+   backend "s3" {
+     bucket         = "chandbjs5fn5sek"
+     key            = "nginx/terraform.tfstate"
+     region         = "us-east-1"
+     dynamodb_table = "terraform-lock"
+     encrypt        = true
+   }
+ }
